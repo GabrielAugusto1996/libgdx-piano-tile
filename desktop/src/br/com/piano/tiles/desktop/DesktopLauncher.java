19af69a823a344b49cd6b9357bd69ae94679f751
Gabriel Augusto <biga00145@gmail.com>
2021-01-24 14:17:10 -0300

    Commit inicial

---
+++ desktop/src/br/com/piano/tiles/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.piano.tiles.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.piano.tiles.MainClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainClass(), config);
+	}
+}
